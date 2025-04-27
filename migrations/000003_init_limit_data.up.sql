adaf97c93ece52fde1be857b05d3d1defe20e715
glennprays <glennsagala@gmail.com>
2025-04-27 22:23:09 +0700

    chore: init consumer limit

---
+++ migrations/000003_init_limit_data.up.sql
@@ -0,0 +1,11 @@
+INSERT INTO consumer_limits (consumer_nik, tenor, limit_amount) VALUES
+('1111', 1, 100000.00),
+('1111', 2, 200000.00),
+('1111', 3, 500000.00),
+('1111', 6, 700000.00);
+
+INSERT INTO consumer_limits (consumer_nik, tenor, limit_amount) VALUES
+('2222', 1, 1000000.00),
+('2222', 2, 1200000.00),
+('2222', 3, 1500000.00),
+('2222', 6, 2000000.00);
